708a4086557c057b1c959077d12511e00db49451
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2026-05-24 11:57:44 +0200

    .

--- ProjectZee.Web/Data/ApplicationDbContext.cs
+++ ProjectZee.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
 namespace ProjectZee.Web.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Project> Projects { get; set; }
+        
     }
 }
---
+++ ProjectZee.Web/Data/Models/Project.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using ProjectZee.Web.Data;
+
+public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RegionOrCity { get; set; } = string.Empty;
+        public string Status { get; set; } = "InAanvraag";
+        public double TargetBudget { get; set; } = 500000;
+        public int Bedrooms { get; set; } = 2;
+        public int MinApartmentSize { get; set; } = 80;
+        public int MaxDistanceToBeach { get; set; } = 100;
+        public RequirementTypes SeaView { get; set; } = RequirementTypes.NiceToHave;
+        public RequirementTypes TerrasRequirement { get; set; } = RequirementTypes.NiceToHave;
+        public int MinTerrasSize { get; set; } = 10;
+        public List<ApplicationUser> Followers { get; set; }
+        public List<ShareHolder> Candidates { get; set; }
+        public List<string> ExtraAttributes { get; set; } = new();
+    }
